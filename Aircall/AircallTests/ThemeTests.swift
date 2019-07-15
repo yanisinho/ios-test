4a9ff0d2906e5fcb0126ad4c2d265ede0899e00a
Yanis SG <ysg.developer@gmail.com>
2019-07-16 00:05:29 +0200

    style: Add UnitTests for Theme.swift

---
+++ Aircall/AircallTests/ThemeTests.swift
@@ -0,0 +1,32 @@
+//
+//  ThemeTests.swift
+//  AircallTests
+//
+//  Created by Yanis SIDAHMED on 16/07/2019.
+//  Copyright © 2019 Yanis SG. All rights reserved.
+//
+
+import XCTest
+@testable import Aircall
+
+class ThemeTests: XCTestCase {
+
+	override func setUp() {
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+	}
+
+	override func tearDown() {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+	}
+
+	/// Test `ThemeTests` colors.
+	func testColors() {
+		// TODO: Implements here
+	}
+
+	/// Test `ColorTests` color names.
+	func testText() {
+		// TODO: Implements here
+	}
+
+}
