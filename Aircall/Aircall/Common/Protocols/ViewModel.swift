138f243610178ed9f7b710018986cbec9caacb2f
Yanis SG <ysg.developer@gmail.com>
2019-07-16 01:35:48 +0200

    feat: Add generic protocol describing ViewModel for MVVM design pattern

---
+++ Aircall/Aircall/Common/Protocols/ViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  ViewModel.swift
+//  Aircall
+//
+//  Created by Yanis SIDAHMED on 16/07/2019.
+//  Copyright © 2019 Yanis SG. All rights reserved.
+//
+
+import Foundation
+
+/**
+
+	Describes an item which can be used as `ViewModel` item.
+
+	Please read [MVVM](https://en.wikipedia.org/wiki/Model–view–viewmodel).
+	This describes what is a ViewModel in MVVM design pattern.
+
+*/
+protocol ViewModel {
+
+	/// `ViewModel`'s input.
+  associatedtype In
+
+  /// `ViewModel`'s output.
+  associatedtype Out
+
+  /**
+
+  	Produce `ViewModel`'s output from `ViewModel`'s input.
+
+  	- parameters:
+  	  - input: `ViewModel` input.
+
+		- returns: `ViewModel` output
+
+  */
+  func setup(from input: In) -> Out
+
+}
