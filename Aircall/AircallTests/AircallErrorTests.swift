6d67dd7a4473d030007dc7fd9525a61ef8a1271f
Yanis SG <ysg.developer@gmail.com>
2019-07-16 00:37:34 +0200

    tests: Add UnitTests for Aircall API errors

---
+++ Aircall/AircallTests/AircallErrorTests.swift
@@ -0,0 +1,27 @@
+//
+//  AircallErrorTests.swift
+//  AircallTests
+//
+//  Created by Yanis SIDAHMED on 16/07/2019.
+//  Copyright © 2019 Yanis SG. All rights reserved.
+//
+
+import XCTest
+@testable import Aircall
+
+class AircallErrorTests: XCTestCase {
+
+	override func setUp() {
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+	}
+
+	override func tearDown() {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+	}
+
+	/// Test `AircallError` cases count.
+	func testCount() {
+		XCTAssertEqual(AircallError.allCases.count, 1)
+	}
+
+}
