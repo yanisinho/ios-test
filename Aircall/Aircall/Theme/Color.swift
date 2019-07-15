a6d78c9e1e81f534179647cedfa85f2bf92a5da7
Yanis SG <ysg.developer@gmail.com>
2019-07-15 23:40:00 +0200

    style: Add Color.swift

---
+++ Aircall/Aircall/Theme/Color.swift
@@ -0,0 +1,29 @@
+//
+//  Color.swift
+//  Aircall
+//
+//  Created by Yanis SIDAHMED on 15/07/2019.
+//  Copyright © 2019 Yanis SG. All rights reserved.
+//
+
+import UIKit
+
+/// Holds all colors available in the app. Colors are defined inside Colors.xcassets
+struct Color {
+
+	/// Primary color.
+	static var primary: UIColor = ColorName.primary.color
+
+	/// Secondary color.
+	static var secondary: UIColor = ColorName.secondary.color
+
+	/// Title color.
+	static var title: UIColor = ColorName.title.color
+
+	/// Subtitle color.
+	static var subtitle: UIColor = ColorName.subtitle.color
+
+	/// Background color.
+	static var background: UIColor = ColorName.background.color
+
+}
