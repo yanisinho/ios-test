a87a89c822f8a71b0b178cdd3f4ac20d54ebe169
Yanis SG <ysg.developer@gmail.com>
2019-07-16 08:50:46 +0200

    feat: Add initial version of CallListModel

--- Aircall/Aircall/Scenes/CallList/Model/CallListModel.swift
+++ Aircall/Aircall/Scenes/CallList/Model/CallListModel.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import CoreData
+
+/// This class is responsable for manipulating the persistence store.
+final class CallListModel {
+
+	/// `CoreData` managed object context.
+	private var managedObjectContext: NSManagedObjectContext
+
+	/// `CoreData` managed object model.
+	private var managedObjectModel: NSManagedObjectModel
+
+	/**
+
+	`CallListModel` custom initializer.
+
+	- Parameters:
+		- managedObjectContext: `CoreData` managed object context to use.
+		- managedObjectModel: `CoreData` managed object model to use.
+
+	- Returns: `CallListModel` initialized and ready to be use.
+
+	*/
+	init(
+		managedObjectContext: NSManagedObjectContext,
+		managedObjectModel: NSManagedObjectModel
+		) {
+		self.managedObjectContext = managedObjectContext
+		self.managedObjectModel = managedObjectModel
+	}
+
+}
