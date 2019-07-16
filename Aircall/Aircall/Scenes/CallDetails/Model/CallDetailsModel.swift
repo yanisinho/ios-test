1fa33889024ed6f37c24898b344d83493e8939f0
Yanis SG <ysg.developer@gmail.com>
2019-07-16 08:52:41 +0200

    feat: Add initial version of CallDetailsModel

--- Aircall/Aircall/Scenes/CallDetails/Model/CallDetailsModel.swift
+++ Aircall/Aircall/Scenes/CallDetails/Model/CallDetailsModel.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import CoreData
+
+/// This class is responsable for manipulating the persistence store.
+struct CallDetailsModel {
+
+	/// Call identifier.
+	let callId: Int
+
+	/// `CoreData` managed object context.
+	fileprivate var managedObjectContext: NSManagedObjectContext
+
+	/// `CoreData` managed object model.
+	fileprivate var managedObjectModel: NSManagedObjectModel
+
+	/**
+
+	`CallListModel` custom initializer.
+
+	- Parameters:
+	  - managedObjectContext: `CoreData` managed object context to use.
+	  - managedObjectModel: `CoreData` managed object model to use.
+	  - callId: Call identifier.
+
+	- Returns: `CallListModel` initialized and ready to be use.
+
+	*/
+	init(
+		managedObjectContext: NSManagedObjectContext,
+		managedObjectModel: NSManagedObjectModel,
+		callId: Int
+		) {
+		self.managedObjectContext = managedObjectContext
+		self.managedObjectModel = managedObjectModel
+		self.callId = callId
+	}
+
+}
