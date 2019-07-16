6f657f503b18941f9dff8b91a617b22274fca7f1
Yanis SG <ysg.developer@gmail.com>
2019-07-16 11:11:35 +0200

    feat: Add CallDetailsModelParameter holding parameters used to retreive CDCall from persistence store

---
+++ Aircall/Aircall/Scenes/CallDetails/Model/CallDetailsParameter.swift
@@ -0,0 +1,33 @@
+//
+//  CallDetailsParameter.swift
+//  Aircall
+//
+//  Created by Yanis SIDAHMED on 16/07/2019.
+//  Copyright © 2019 Yanis SG. All rights reserved.
+//
+
+import Foundation
+
+/// Search parameters.
+struct CallDetailsModelParameter {
+
+	/// Call identifier.
+	let callId: Int
+
+	/**
+
+	`CallDetailsModelParameter` custom initializer from parameters.
+
+	- Parameters:
+		- callId: Call identifier.
+
+	- Returns: `CallDetailsModelParameter` initialized and ready to use.
+
+	*/
+	init(
+		callId: Int
+	) {
+		self.callId = callId
+	}
+
+}
