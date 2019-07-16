f01ee686f8b239616d93d541b4fc17bc733bca3d
Yanis SG <ysg.developer@gmail.com>
2019-07-16 11:09:24 +0200

    feat: Update CDCall from CallDetailsModel

--- Aircall/Aircall/Scenes/CallDetails/Model/CallDetailsModel.swift
+++ Aircall/Aircall/Scenes/CallDetails/Model/CallDetailsModel.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Yanis SG. All rights reserved.
 //
 
+import UIKit
 import Foundation
 import CoreData
 
@@ -41,6 +42,43 @@ struct CallDetailsModel {
 		self.managedObjectContext = managedObjectContext
 		self.managedObjectModel = managedObjectModel
 		self.callId = callId
+	}
+
+}
+
+// MARK: - Update
+
+extension CallDetailsModel {
+
+	/**
+
+	Update `CDCall` entitiy inside persistence store.
+
+	- Parameters:
+	  - call: Call to persist.
+
+	*/
+	func update(call: WSCall) {
+		guard let newCall = NSEntityDescription.insertNewObject(
+			forEntityName: "CDCall",
+			into: managedObjectContext
+			) as? CDCall else {
+				print("❤️ CoreData: Couldn't insert new CDCall in persistence store.")
+				return
+		}
+		newCall.id = Int32(call.id)
+		newCall.created = call.created as NSDate
+		newCall.direction = call.direction
+		newCall.from = call.from
+		newCall.to = call.to
+		newCall.via = call.via
+		newCall.duration = Int32(call.duration)
+		newCall.type = call.type
+		newCall.isArchived = call.isArchived
+		guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+			fatalError("AppDelegate couldn't be retreived.")
+		}
+		appDelegate.saveContext()
 	}
 
 }
