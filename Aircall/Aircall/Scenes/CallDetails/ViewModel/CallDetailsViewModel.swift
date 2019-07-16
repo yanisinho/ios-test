c1ab8ed5bf0427a88bf5821dc3b158d412080ac1
Yanis SG <ysg.developer@gmail.com>
2019-07-16 11:38:41 +0200

    feat: Add properties and initializer for CallDetailsViewModel

--- Aircall/Aircall/Scenes/CallDetails/ViewModel/CallDetailsViewModel.swift
+++ Aircall/Aircall/Scenes/CallDetails/ViewModel/CallDetailsViewModel.swift
@@ -9,6 +9,7 @@
 import RxSwift
 import RxCocoa
 import Foundation
+import Moya
 
 final class CallDetailsViewModel: ViewModel {
 
@@ -56,7 +57,45 @@ final class CallDetailsViewModel: ViewModel {
 
 	}
 
-		/**
+	// MARK: - Properties
+
+	/// Coordinator
+	weak var coordinator: CallDetailsCoordinator?
+
+	/// Model
+	private let model: CallDetailsModel
+
+	/// Moya provider used for web services.
+	private let provider: MoyaProvider<Aircall>
+
+	/// JSONDecoder used with web services.
+	private let decoder: JSONDecoder
+
+	/// RxSwift dispose bag
+	private let disposeBag = DisposeBag()
+
+	/// Emits an event when call is updated.
+	private var successBehavior: BehaviorRelay<WSCall?>
+
+	/// Emits an event when table view refreshing status changed.
+	private var errorBehavior: BehaviorRelay<WSError?>
+
+
+	init(
+		provider: MoyaProvider<Aircall>,
+		decoder: JSONDecoder,
+		coordinator: CallDetailsCoordinator,
+		model: CallDetailsModel
+		) {
+		self.provider = provider
+		self.decoder = decoder
+		self.coordinator = coordinator
+		self.model = model
+		self.errorBehavior = BehaviorRelay(value: nil)
+		self.successBehavior = BehaviorRelay(value: nil)
+	}
+
+	/**
 
 	Produce `ViewModel`'s output from `ViewModel`'s input.
 
@@ -69,7 +108,7 @@ final class CallDetailsViewModel: ViewModel {
 	func setup(
 		from input: CallDetailsViewModel.Input
 		) -> CallDetailsViewModel.Output {
-		
+
 	}
 
 }
