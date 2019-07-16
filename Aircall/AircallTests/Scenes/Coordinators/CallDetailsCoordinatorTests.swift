0fb800d99f8346ad6045e263c4b508b5d74c4040
Yanis SG <ysg.developer@gmail.com>
2019-07-16 13:21:02 +0200

    tests: Add CallDetailsCoordinator UnitTests

--- Aircall/AircallTests/Scenes/Coordinators/CallDetailsCoordinatorTests.swift
+++ Aircall/AircallTests/Scenes/Coordinators/CallDetailsCoordinatorTests.swift
@@ -6,4 +6,74 @@
 //  Copyright © 2019 Yanis SG. All rights reserved.
 //
 
-import Foundation
+import XCTest
+import Moya
+import CoreData
+@testable import Aircall
+
+class CallDetailsCoordinatorTests: XCTestCase {
+
+	override func setUp() {
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+	}
+
+	override func tearDown() {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+	}
+
+	/// Test `CallDetailsCoordinator` initializer.
+	func testInit() {
+
+		// Create dummy view controllers.
+		let firstViewController = UIViewController()
+		let secondViewController = UIViewController()
+		let thirdViewController = UIViewController()
+
+		// Create dummy navigation controller.
+		let navigationController = UINavigationController()
+		navigationController.pushViewController(firstViewController, animated: false)
+		navigationController.pushViewController(secondViewController, animated: false)
+		navigationController.pushViewController(thirdViewController, animated: false)
+
+		// Create dummy managed object context.
+		let managedObjectModel = NSManagedObjectModel.mergedModel(from: [Bundle.main])!
+
+		// Create persistent store coordinator.
+		let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+		do {
+			try persistentStoreCoordinator.addPersistentStore(ofType: NSInMemoryStoreType, configurationName: nil, at: nil, options: nil)
+		} catch {
+			print("Adding in-memory persistent store failed")
+		}
+
+		// Create dummy managed object model.
+		let managedObjectContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+		managedObjectContext.persistentStoreCoordinator = persistentStoreCoordinator
+
+		// Create dummy moya provider.
+		let provider: MoyaProvider = MoyaProvider<Aircall>()
+
+		// Create dummy json decoder.
+		let decoder: JSONDecoder = JSONDecoder()
+		decoder.dataDecodingStrategy = .base64
+		decoder.dateDecodingStrategy = .iso8601
+		decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+		// Create dummy call identifier.
+		let callId: Int = 666
+
+		// Create dummy coordinator
+		let coordinator = CallDetailsCoordinator(
+			navigationController: navigationController,
+			managedObjectContext: managedObjectContext,
+			managedObjectModel: managedObjectModel,
+			provider: provider,
+			decoder: decoder,
+			callId: callId
+		)
+
+		XCTAssertEqual(coordinator.navigationController.children.count, 3)
+		XCTAssertEqual(coordinator.callId, 666)
+	}
+
+}
