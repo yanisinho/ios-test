ce3e30a02fa7094649477c90d206d4be44cc4677
Yanis SG <ysg.developer@gmail.com>
2019-07-16 09:03:04 +0200

    feat: Use AppCoordinator from AppDelegate

--- Aircall/Aircall/AppDelegate.swift
+++ Aircall/Aircall/AppDelegate.swift
@@ -12,8 +12,15 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+	// MARK: - Properties
+
+	/// Application coordinator.
+	var coordinator: AppCoordinator?
+
 	/// Main window.
 	var window: UIWindow?
+
+	// MARK: - Lifecycle
 
 	func application(
 		_ application: UIApplication,
@@ -22,6 +29,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 		// Configure app theme.
 		Theme()
+
+		/// Configure AppCoordinator
+		let navigationController = UINavigationController()
+		coordinator = AppCoordinator(
+			navigationController: navigationController,
+			managedObjectContext: persistentContainer.viewContext,
+			managedObjectModel: persistentContainer.managedObjectModel
+		)
+		coordinator?.start()
+		window = UIWindow(frame: UIScreen.main.bounds)
+		window?.rootViewController = navigationController
+		window?.makeKeyAndVisible()
 
 		return true
 	}
@@ -49,50 +68,51 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	// MARK: - Core Data stack
 
 	lazy var persistentContainer: NSPersistentContainer = {
-	    /*
-	     The persistent container for the application. This implementation
-	     creates and returns a container, having loaded the store for the
-	     application to it. This property is optional since there are legitimate
-	     error conditions that could cause the creation of the store to fail.
-	    */
-	    let container = NSPersistentContainer(name: "Aircall")
-	    container.loadPersistentStores(completionHandler: { ( _, error) in
-	        if let error = error as NSError? {
-	            // Replace this implementation with code to handle the error appropriately.
-	            // fatalError() causes the application to generate a crash log and terminate.
-	            // You should not use this function in a shipping application,
-	            // although it may be useful during development.
+		/*
+		The persistent container for the application. This implementation
+		creates and returns a container, having loaded the store for the
+		application to it. This property is optional since there are legitimate
+		error conditions that could cause the creation of the store to fail.
+		*/
+		let container = NSPersistentContainer(name: "Aircall")
+		container.loadPersistentStores(completionHandler: { ( _, error) in
+			container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+			if let error = error as NSError? {
+				// Replace this implementation with code to handle the error appropriately.
+				// fatalError() causes the application to generate a crash log and terminate.
+				// You should not use this function in a shipping application,
+				// although it may be useful during development.
 
-	            /*
-	             Typical reasons for an error here include:
-	             * The parent directory does not exist, cannot be created, or disallows writing.
-	             * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-	             * The device is out of space.
-	             * The store could not be migrated to the current model version.
-	             Check the error message to determine what the actual problem was.
-	             */
-	            fatalError("Unresolved error \(error), \(error.userInfo)")
-	        }
-	    })
-	    return container
+				/*
+				Typical reasons for an error here include:
+				* The parent directory does not exist, cannot be created, or disallows writing.
+				* The persistent store is not accessible, due to permissions or data protection when the device is locked.
+				* The device is out of space.
+				* The store could not be migrated to the current model version.
+				Check the error message to determine what the actual problem was.
+				*/
+				fatalError("Unresolved error \(error), \(error.userInfo)")
+			}
+		})
+		return container
 	}()
 
 	// MARK: - Core Data Saving support
 
 	func saveContext () {
-	    let context = persistentContainer.viewContext
-	    if context.hasChanges {
-	        do {
-	            try context.save()
-	        } catch {
-	            // Replace this implementation with code to handle the error appropriately.
-	            // fatalError() causes the application to generate a crash log and terminate.
-	            // You should not use this function in a shipping application,
-	            // although it may be useful during development.
-	            let nserror = error as NSError
-	            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-	        }
-	    }
+		let context = persistentContainer.viewContext
+		if context.hasChanges {
+			do {
+				try context.save()
+			} catch {
+				// Replace this implementation with code to handle the error appropriately.
+				// fatalError() causes the application to generate a crash log and terminate.
+				// You should not use this function in a shipping application,
+				// although it may be useful during development.
+				let nserror = error as NSError
+				fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+			}
+		}
 	}
 
 }
